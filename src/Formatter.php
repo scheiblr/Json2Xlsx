577a6b544c24ec1685b5abffbf4d9b2384c016b9
Raphael Scheible <raphael.scheible@uniklinik-freiburg.de>
2018-08-24 18:55:33 +0200

    working now

--- src/Formatter.php
+++ src/Formatter.php
@@ -18,7 +18,7 @@ class Formatter
         }
     }
 
-    static function titleToRows($titles, $funcIsHeadline = UKLFR\Json2Xlsx\isHeadline)
+    static function titleToRows($titles, $funcIsHeadline = ['UKLFR\Json2Xlsx\Formatter', 'isHeadline'])
     {
         $result = [0 => []];
 
@@ -31,7 +31,7 @@ class Formatter
         return $result;
     }
 
-    private static function titleToRowsHelp(&$array, $titles, &$col, $row, $depth = 0, $funcIsHeadline = UKLFR\Json2Xlsx\isHeadline)
+    private static function titleToRowsHelp(&$array, $titles, &$col, $row, $depth = 0, $funcIsHeadline)
     {
         foreach ($titles as $key => $title) {
             if (empty($array[$row])) {
@@ -41,15 +41,15 @@ class Formatter
             // recursion call
             if (is_array($title)) {
                 // regular step
-                if (!is_numeric($key) && !$funcIsHeadline($key)) {
+                if (!is_numeric($key) && !call_user_func($funcIsHeadline, $key)) {
                     $array[$row][$col] = $key;
                 }
                 $row++;
-                self::titleToRowsHelp($array, $title, $col, $row, $depth + 1);
+                self::titleToRowsHelp($array, $title, $col, $row, $depth + 1, $funcIsHeadline);
                 $row--;
             } else {
                 // regular step
-                if (!$funcIsHeadline($title)) {
+                if (!call_user_func($funcIsHeadline, $title)) {
                     $array[$row][$col++] = $title;
                 }
             }
@@ -67,7 +67,7 @@ class Formatter
 
         foreach ($array as $value) {
             if (is_array($value)) {
-                $depth = array_depth($value) + 1;
+                $depth = self::array_depth($value) + 1;
 
                 if ($depth > $max_depth) {
                     $max_depth = $depth;
@@ -78,16 +78,79 @@ class Formatter
         return $max_depth;
     }
 
-    static function fillSheet(&$objSheet, $sheetName, $patients, $freezeCols = 1, $funcIsHeadline = UKLFR\Json2Xlsx\isHeadline)
+    static function entitiyToRow($entity, $cols, $funcIsHeadline = ['UKLFR\Json2Xlsx\Formatter', 'isHeadline'])
+    {
+        $maxEntriesPerCol = 1;
+        foreach ($entity as $field) {
+            if (is_array($field)) {
+                $maxEntriesPerCol = max($maxEntriesPerCol, sizeof($field));
+            }
+        }
+
+        // create array
+        $result = array_fill(0, $maxEntriesPerCol, array_fill(0, $cols, NULL));
+
+        $patientNumeric = array_values($entity);
+
+        // write data
+        $col = 0;
+
+        foreach ($entity as $key => $field) {
+            $offsetRow = 0;
+            // if is array, which means there are
+            // several entries in that field
+            if (is_array($field)) {
+                // print all these entries inside the same col
+                foreach ($field as $elem) {
+                    // if element is an array
+                    if (is_array($elem)) {
+                        // each element results to be one row
+                        $offsetCol = 0;
+                        foreach ($elem as $value) {
+                            // set value
+                            if (!is_null($value)) {
+                                $result[$offsetRow][$col + $offsetCol] = $value;
+                            }
+
+                            // increment offset col
+                            ++$offsetCol;
+                        }
+                        // increment offset row
+                        ++$offsetRow;
+//                    $result[$offsetRow++][$col] = json_encode($elem);
+                    } else {
+                        $result[$offsetRow][$col++] = $elem;
+                    }
+                }
+
+                // increment cols only one time
+                $col += sizeof($field[0]);
+            } else {
+                if (!call_user_func($funcIsHeadline, $key)) {
+                    $result[0][$col++] = $field;
+                }
+            }
+        }
+
+        // fill id fields respectevily
+        for ($i = 1; $i < sizeof($result); ++$i) {
+            $result[$i][0] = $result[$i - 1][0];
+            $result[$i][1] = $result[$i - 1][1];
+        }
+
+        return $result;
+    }
+    
+    static function fillSheet(&$objSheet, $sheetName, $entities, $freezeCols = 1, $funcIsHeadline = ['UKLFR\Json2Xlsx\Formatter', 'isHeadline'])
     {
         // get headlines
         $headlines = [];
         $i = 0;
         $titles = [];
 
-        if (count($patients) > 0) {
-            foreach ($patients[0] as $key => $value) {
-                if ($funcIsHeadline($key)) {
+        if (count($entities) > 0) {
+            foreach ($entities[0] as $key => $value) {
+                if (call_user_func($funcIsHeadline, $key)) {
                     $headlines[$i] = $value;
                 } else {
                     if (is_array($value)) {
@@ -99,7 +162,7 @@ class Formatter
             }
 
             // estimate titles
-            foreach ($patients[0] as $key => $field) {
+            foreach ($entities[0] as $key => $field) {
                 if (is_array($field)) {
                     $subtitles = [];
                     foreach ($field[0] as $subkey => $subfield) {
@@ -116,9 +179,9 @@ class Formatter
         // setup excel sheet
         $objSheet->setTitle($sheetName);
 
-        $grid = titleToRows($titles);
+        $grid = self::titleToRows($titles);
 
-        $depth = array_depth($titles) + ($headlines !== []);
+        $depth = self::array_depth($titles) + ($headlines !== []);
 
         // freeze rows
         $objSheet->freezePaneByColumnAndRow($freezeCols, $depth + 1);
@@ -132,8 +195,8 @@ class Formatter
 
 
 //    println('xml 2 array');
-        foreach ($patients as $patient) {
-            $rows = patientToRow($patient, $cols);
+        foreach ($entities as $entity) {
+            $rows = self::entitiyToRow($entity, $cols);
 
             foreach ($rows as $row) {
                 array_push($grid, $row);
