96ecc6539ecce149b203c15c7591ae5e83341351
Raphael Scheible <raphael.scheible@uniklinik-freiburg.de>
2018-08-24 19:58:15 +0200

    major fixes

--- src/Fetcher.php
+++ src/Fetcher.php
@@ -4,78 +4,66 @@ namespace UKLFR\Json2Xlsx;
 
 class Fetcher
 {
-    static function getDataFromDB($config, $schema, $view, $db_settings = false)
-    {
-        // setting up PDO
-        $dbLocation = "{$config['type']}:dbname={$config['db']};host={$config['host']}";
-        $db = new PDO($dbLocation, $config['user'], $config['password'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+    static function dbConnect($config, $db_settings = false) {
+        try {
+            $dbLocation = "{$config['type']}:dbname={$config['db']};host={$config['host']}";
+            $conn = new \PDO($dbLocation, $config['user'], $config['password'], array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_WARNING));
 
+            self::applyDbSettings($conn, $db_settings);
+
+            return $conn;
+        } catch (\PDOException $e) {
+            echo 'PDO Error: ' . $e->getMessage();
+            return false;
+        }
+    }
+
+    static function applyDbSettings($conn, $db_settings) {
         if ($db_settings) {
             if (is_array($db_settings)) {
                 foreach ($db_settings as $setting) {
-                    $db->exec($setting);
+                    $conn->exec($setting);
                 }
             } else {
-                $db->exec($db_settings);
+                $conn->exec($db_settings);
             }
         }
+    }
 
-
-        $qry = $db->prepare("SELECT * FROM {$schema}.{$view}");
+    static function query($conn, $query, $fieldname = 'export') {
+        $qry = $conn->prepare($query);
         $qry->execute();
 
         // since it is a 0..1 to 1 relation
         // if no data set (i.e. 0-relation) return empty array
         // the first tuple else (1-relation)
-        $qryResult = $qry->fetch(PDO::FETCH_ASSOC);
-
+        $qryResult = $qry->fetch(\PDO::FETCH_ASSOC);
 
         //Decode JSON
-        $result = json_decode($qryResult['export'], true);
+        $result = json_decode($qryResult[$fieldname], true);
 
         // free memory
         unset($qryResult);
-
         $qry = null;
-        $db = null;
 
         return $result;
     }
 
-    static function getDataFromDBFunc($config, $schema, $function, $param, $db_settings = false)
+    static function getDataFromDB($conn, $schema, $view, $db_settings = false)
     {
-        // setting up PDO
-        $dbLocation = "{$config['type']}:dbname={$config['db']};host={$config['host']}";
-        $db = new PDO($dbLocation, $config['user'], $config['password'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+        // apply query specific db_settings
+        self::applyDbSettings($conn, $db_settings);
 
-        if ($db_settings) {
-            if (is_array($db_settings)) {
-                foreach ($db_settings as $setting) {
-                    $db->exec($setting);
-                }
-            } else {
-                $db->exec($db_settings);
-            }
-        }
+        // perform query
+        return self::query($conn, "SELECT * FROM {$schema}.{$view}");
+    }
 
-        $qry = $db->prepare("SELECT $schema.$function($param) as export;");
-        $qry->execute();
+    static function getDataFromDBFunc($conn, $schema, $function, $param, $db_settings = false)
+    {
+        // apply query specific db_settings
+        self::applyDbSettings($conn, $db_settings);
 
-        // since it is a 0..1 to 1 relation
-        // if no data set (i.e. 0-relation) return empty array
-        // the first tuple else (1-relation)
-        $qryResult = $qry->fetch(PDO::FETCH_ASSOC);
-
-
-        //Decode JSON
-        $result = json_decode($qryResult['export'], true);
-
-        // free memory
-        unset($qryResult);
-
-        $qry = null;
-        $db = null;
-
-        return $result;
+        // perform query
+        return  self::query($conn, "SELECT $schema.$function($param) as export;");
     }
 }
