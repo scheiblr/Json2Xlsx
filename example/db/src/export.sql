0d469c4794b287b17eb6cc24da8034da69d988f7
Raphael Scheible <raphael.scheible@uniklinik-freiburg.de>
2018-08-28 12:24:15 +0200

    fixed: duplicates removed

--- example/db/src/export.sql
+++ example/db/src/export.sql
@@ -1,15 +1,16 @@
 -- returns value tree given a documentation date id
+-- use JSONB here in order to make it compatible with DISTINCT (below)
 create or replace function f_get_values(_document_id INT)
-  returns json as $$
-select coalesce(json_agg(tmp),
+  returns jsonb as $$
+select coalesce(jsonb_agg(tmp),
   '[
     {
       "name": null,
       "value": null
     }
-  ]' :: JSON)
+  ]' :: JSONB)
   from
-  (select
+  (select distinct
     name,
     value
   from
@@ -21,19 +22,20 @@ select coalesce(json_agg(tmp),
 $$ language sql;
 
 -- exports documents tree given a patient_id
+-- use JSONB here in order to make it compatible with DISTINCT (below)
 create or replace function f_get_documents(_patient_id INT)
-returns json as $$
+returns jsonb as $$
 -- if there is no tuple, return the empty entity
-select coalesce(json_agg(tmp),
+select coalesce(jsonb_agg(tmp),
   '[
     {
       "document_id": null,
       "visit_date": null,
       "to_delete": null
     }
-  ]' :: JSON)
+  ]' :: JSONB)
   from
-  (select
+  (select distinct
     document_id,
     visit_date,
     to_delete
@@ -45,15 +47,18 @@ select coalesce(json_agg(tmp),
 $$ language sql;
 
 -- view which contains all data
+-- use DISTINCT to remove duplicates
 create or replace view v_all_data as
-select
+select distinct
   'patient data' as "headline_p",
   patient_id,
   lastname,
   firstname,
   deceased,
+
   'documentation dates' as "headline_dd",
   f_get_documents(patient_id) as documents,
+
   'lab values' as "headline_lv",
   f_get_values(document_id) as lab_values
 from patient
@@ -61,17 +66,22 @@ from patient
 order by patient_id;
 
 -- data export view
+-- use JSON here in order to keep the order of columns
 create or replace view v_export as
 select coalesce(json_agg(tmp), '[]' :: JSON) AS export
 from (select * from v_all_data)
 as tmp;
 
 -- data export function
+-- use JSON here in order to keep the order of columns
 create or replace function f_export(INT [])
-  returns setof v_all_data
+  returns JSON
 as
 $$
-select *
-from v_all_data
-  where patient_id = ANY($1)
+select coalesce(json_agg(tmp),
+  '[]' :: JSON)
+  from
+  (select *
+  from v_all_data
+    where patient_id = ANY($1)) as tmp
 $$ language sql;
